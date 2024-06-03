c5c1902ee2322d4da198a5786a16fb893a2e69fd
Daylina1991 <tapiamaday1991@gmail.com>
2024-06-03 20:57:53 -0300

    commit

--- bbdd_.py
+++ bbdd_.py
@@ -6,42 +6,38 @@ micursor = miconexion.cursor()
 micursor.execute("drop database  if exists proyecto_bbdd")
 micursor.execute("create database proyecto_bbdd")
 micursor.execute("use proyecto_bbdd")
-micursor.execute("create table clientes (id int primary key auto_increment, codigo text (50),nombre text (50), direccion text (50), telefono text(50), situacion text (50))")
-micursor.execute("create table peliculas(id int primary key auto_increment, codigo text (50),nombre_peli text (50),genero text(50),situacion text (50))")
+micursor.execute("create table clientes (id int primary key auto_increment, codigo text (50),nombre text (50), direccion text (50), telefono text(50), situacion text (50), codigo_peli text (50) null)")
+micursor.execute("create table peliculas(id int primary key auto_increment, codigo text (50),nombre_peli text (50),genero text(50),situacion text (50), codigo_cliente text(50) null)")
 
-sql = "INSERT INTO clientes(codigo, nombre, direccion, telefono, situacion) VALUES (%s, %s, %s, %s, %s)"
+sql = "INSERT INTO clientes(codigo, nombre, direccion, telefono, situacion, codigo_peli) VALUES (%s, %s, %s, %s, %s, %s)"
 val =[
-    ("C001", "Juan", "Calle Falsa 123", "34151234", "l"),
-    ("C002", "María", "Avenida Siempre Viva 742", "34155678", "l"),
-    ("C003", "Carlos", "Calle Principal 456", "34158765", "a"),
-    ("C004", "Ana", "Boulevard de los Sueños 890", "34154321", "l"),
-    ("C005", "Pedro", "Calle Secundaria 101", "34156789", "l"),
-    ("C006", "Luisa", "Avenida de la Paz 102", "34159876", "a"),
-    ("C007", "Miguel", "Calle de la Amistad 203", "341534562", "a"),
-    ("C008", "Laura", "Calle de la Esperanza 304", "34156543", "l"),
-    ("C009", "Jorge", "Avenida del Sol 405", "34157890", "a"),
-    ("C010", "Sofia", "Calle de las Flores 506", "34150987", "l")
+    ("C001", "Juan", "Calle Falsa 123", "34151234", "l","null"),
+    ("C002", "María", "Avenida Siempre Viva 742", "34155678", "l","null"),
+    ("C003", "Carlos", "Calle Principal 456", "34158765", "l","null"),
+    ("C004", "Ana", "Boulevard de los Sueños 890", "34154321", "l","null"),
+    ("C005", "Pedro", "Calle Secundaria 101", "34156789", "l","null"),
+    ("C006", "Luisa", "Avenida de la Paz 102", "34159876", "l","null"),
+    ("C007", "Miguel", "Calle de la Amistad 203", "341534562", "l","null"),
+    ("C008", "Laura", "Calle de la Esperanza 304", "34156543", "l","null"),
+    ("C009", "Jorge", "Avenida del Sol 405", "34157890", "l","null"),
+    ("C010", "Sofia", "Calle de las Flores 506", "34150987", "l","null")
 ]
 micursor.executemany(sql, val )
 miconexion.commit() 
 
-sql = "INSERT INTO peliculas(codigo, nombre_peli, genero, situacion) VALUES (%s, %s, %s, %s)"
+sql = "INSERT INTO peliculas(codigo, nombre_peli, genero, situacion, codigo_cliente) VALUES (%s, %s, %s, %s, %s)"
 val =[
-    ("C001", "Inception", "Ciencia ficción", "a"),
-    ("C002", "Titanic", "Romance", "a"),
-    ("C003", "The Matrix", "Acción", "l"),
-    ("C004", "Gladiator", "Drama", "l"),
-    ("C005", "Avatar", "Ciencia ficción", "l"),
-    ("C006", "The Lion King", "Animación", "l"),
-    ("C007", "Forrest Gump", "Drama", "a"),
-    ("C008", "The Godfather", "Crimen", "l"),
-    ("C009", "Star Wars", "Ciencia ficción", "l"),
-    ("C010", "Jurassic Park", "Aventura", "l")
+    ("C001", "Inception", "Ciencia ficción", "l","null"),
+    ("C002", "Titanic", "Romance", "l","null"),
+    ("C003", "The Matrix", "Acción", "l","null"),
+    ("C004", "Gladiator", "Drama", "l","null"),
+    ("C005", "Avatar", "Ciencia ficción", "l","null"),
+    ("C006", "The Lion King", "Animación", "l","null"),
+    ("C007", "Forrest Gump", "Drama", "a","null"),
+    ("C008", "The Godfather", "Crimen", "l","null"),
+    ("C009", "Star Wars", "Ciencia ficción", "l","null"),
+    ("C010", "Jurassic Park", "Aventura", "l","null")
 ]
 micursor.executemany(sql, val )
 miconexion.commit()
-
-"""micursor.execute("UPDATE clientes SET nombre = 'Helio' WHERE id =1")
-micursor.execute("DELETE  FROM peliculas WHERE id = 2")
-micursor.execute("SELECT  nombre FROM  peliculas")"""
 
---
+++ obj_bbdd.py
@@ -0,0 +1,96 @@
+from bbdd_ import miconexion, micursor
+
+
+
+#clase user                     
+#atributos                    
+#metodos                        
+#ver todas las pelis               select
+#ver solos disponibles             select where
+#alquiler/devolver                 update
+
+
+#darce de alta                     insert
+# darce de baja                    delete
+# ver su datos                     select
+# modificar telf/direc             update
+"""micursor.execute("UPDATE clientes SET nombre = 'Helio' WHERE id =1")
+micursor.execute("DELETE  FROM peliculas WHERE id = 2")
+micursor.execute("SELECT  nombre FROM  peliculas")"""
+
+
+class usuario():
+    def __init__(self, codigo, nombre, direccion, telefono, situacion, codigo_peli):
+        self.__codigo =codigo
+        self.__nombre = nombre
+        self.__direccion = direccion
+        self.__telefono = telefono
+        self.__situacion = situacion 
+        self.__codigo_peli = codigo_peli
+
+    def get_codigo(self):
+        return self.__codigo
+    def set_codigo(self,newcodigo):
+        self.__codigo = newcodigo
+
+    def get_nombre(self):
+        return self.__nombre
+    def set_nombre(self, newnombre):
+        self.__nombre = newnombre
+
+    def get_direccion(self):
+        return self.__direccion
+    def set_direccion(self, newdireccion):
+        self.__direccion = newdireccion
+
+    def get_telefono(self):
+        return self.__telefono
+    
+    def set_telefono(self,newtelefono):
+        self.__telefono = newtelefono
+
+    def get_situacion(self):
+        return self.__situacion
+    def set_situacion(self, newsituacion):
+        self.__situacion = newsituacion
+
+    def get_codigo_peli(self):
+        return self.__codigo_peli
+    def set_codigo(self, newcodigo_peli):
+        self.__codigo_peli = newcodigo_peli
+    
+    def darce_de_alta(self):
+        micursor.execute("insert into from clientes (codigo, nombre, direccion, telefono, situacion, codigo_peli) values ")
+    val = ("C0011", "Dayana","Calle falsa 364", "64388378", "l","null")
+    micursor.execute(val)
+    miconexion.commit()
+        
+
+    def darce_de_baja(self):
+        micursor.execute("delete from clientes where id = 4")
+        resultado =micursor.fetchall()
+    def ver_sus_datos(self):
+        micursor.execute("select  * from clientes  where id= 3")
+    def modificar_datos(self):
+        micursor.execute("update  clientes set nombre = 'Maria' where id = 3 ")
+
+    def ver_todas_las_pelis (self):
+        micursor.execute("select * from peliculas ")
+        resultado =micursor.fetchall()
+        for i in resultado:
+            print(i)
+
+    def ver_solo_disponibles(self):
+        micursor.execute("select * from peliculas where  situacion = 'l'")
+        resultado =micursor.fetchall()
+        for i in resultado:
+            print(i)
+
+    def alquilar_peli  (self):
+        micursor.execute("")
+
+    def devolver_peli (self):
+        pass        
+
+objeto = usuario()
+objeto.ver_todas_las_pelis()
