20020a394f6b8275485f71b2c44ba7771e2e43d9
dayana <daylinatapia1991@gmail.com>
2024-06-10 00:42:41 -0300

    actualizando

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
-def menu():
-    while True:
-        print("\nGestión de Clientes")
-        print("1. crear usuario")
-        print("2. eliminar usuario")
-        print("3. Eliminar usuario")
-        print("4. Actualizar usuario")
-        print("5. Salir")
-        usuario = input("Seleccione una opción: ")
-        if usuario == 1:
-            codigo = int(input("codigo "))
-            nombre = str(input("nombre: "))
-            direccion = str(input("dirección: "))
-            telefono = int(input("teléfono: "))
-            situacion = str(input("situacion: "))
-            codigo_peli = int(input("codigo_peli: "))
-        elif usuario == 2:
-            
-        elif usuario == 3:
-            id = int(input("ID del cliente a eliminar: "))
-        elif usuario== 4:
-            id = int(input("ID del cliente a actualizar: "))
-            codigo = input("nuevo código: ")
-            nombre = input("nuevo nombre: ")
-            direccion = input("nueva dirección: ")
-            telefono = input("nuevo teléfono: ")
-            situacion = input("nueva situacion: ")
-        elif usuario == 5:
-            break
-        else:
-            print("Opción no válida, por favor intente de nuevo.")
 
-if __name__ == "__main__":
-    menu()
+from obj import obj
+def menuPrincipal():
+    on = 1
+    while on == 1:
+        print("\n BIENVENIDO A LA APLICACION DE VIDEO/CLUB")
+        try:
+            opcion =int(input("Opciones: 1-Menu de usuario\n2-Menu peliculas\n3-Salir "))
+            if opcion == 1:
+                print("Este es el menu usuario ")
+            elif opcion == 2:
+                print("Este es el menu Peliculas")
+            elif opcion == 3:
+                print("Gracias por usar la aplicacion")
+                quit()
+            else:
+                print("Opcion incorrecta ")
+        except ValueError:
+            print("Debe ingresar un numero entero ")
+def menuUsuario():
+    try:
+        print("\n Este es el menu usuario")
+        usuario = int(input("Opciones \n 1- Ver datos\n2-Modificar datos\nDarse de baja "))
+            if usuario == 1:
+                codigo = int(input("codigo "))
+                nombre = str(input("nombre: "))
+                direccion = str(input("dirección: "))
+                telefono = int(input("teléfono: "))
+                situacion = str(input("situacion: "))
+                codigo_peli = int(input("codigo_peli: "))
+            elif usuario == 2:
+                 id = int(input("ID del cliente a eliminar: "))
+                
+            elif usuario  == 3:
+                pass
+            else:
+                pass
+    except ValueError:
+            print("Debe ingresar un numero entero  hola")  
